f084a881274fa2f9dd26964c1f2e450b74bb5c05
kallip <kallinpettit@gmail.com>
2018-07-27 18:30:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,12 @@
 class Dog
     def initialize(name)
     @name = name
-  end
+end
 end
   
-  fido = Dog.new ("fido")
-  snoopy = Dog.new ("snoopy")
-  lassie = Dog.new ("lassie")
-end
+fido = Dog.new ("fido")
+snoopy = Dog.new ("snoopy")
+lassie = Dog.new ("lassie")
+
 
   
