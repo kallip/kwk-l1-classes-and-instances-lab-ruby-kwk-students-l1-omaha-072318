f5ae8791683da302c807deaa634331f9dca610cb
kallip <kallinpettit@gmail.com>
2018-07-27 18:29:44 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@
 class Person
   def initialize(name)
     @name = name
-  end
 end
-  adele_goldberg = Person.new("adele_goldberg")
-  alan_kay = Person.new("alan_kay")
+end
+adele_goldberg = Person.new("adele_goldberg")
+alan_kay = Person.new("alan_kay")
